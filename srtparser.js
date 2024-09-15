ff93e5a0184f68a7c04feab1011c2ebeffca280c
Nicolas Gabriel <baigernicolas@gmail.com>
2024-09-15 19:21:12 -0300

    send v1.0

--- srtparser.js
+++ srtparser.js
@@ -18,54 +18,218 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _SrtParser_input, _SrtParser_caption;
+var _SrtParser_instances, _SrtParser_input, _SrtParser_current, _SrtParser_caption, _SrtParser_tracks, _SrtParser__stdout, _SrtParser_media, _SrtParser_track_lists, _SrtParser_Process;
 export class SrtParser {
-    constructor(input) {
+    constructor() {
+        _SrtParser_instances.add(this);
         _SrtParser_input.set(this, void 0);
+        _SrtParser_current.set(this, void 0);
         _SrtParser_caption.set(this, new Map());
-        __classPrivateFieldSet(this, _SrtParser_input, input, "f");
+        _SrtParser_tracks.set(this, new Map());
+        _SrtParser__stdout.set(this, void 0);
+        _SrtParser_media.set(this, void 0);
+        _SrtParser_track_lists.set(this, []);
     }
-    lexer() {
-        const input = __classPrivateFieldGet(this, _SrtParser_input, "f");
+    set stdout(elem) {
+        __classPrivateFieldSet(this, _SrtParser__stdout, elem, "f");
+    }
+    SetCaptions(TrackObject) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                TrackObject.forEach((Track) => __awaiter(this, void 0, void 0, function* () {
+                    console.log(Track.url);
+                    const res = yield this.request(Track.url);
+                    __classPrivateFieldSet(this, _SrtParser_input, res, "f");
+                    let data = __classPrivateFieldGet(this, _SrtParser_instances, "m", _SrtParser_Process).call(this);
+                    const media = __classPrivateFieldGet(this, _SrtParser_media, "f");
+                    const TEXT_T = media.addTextTrack("captions", Track.label, Track.language);
+                    const length = __classPrivateFieldGet(this, _SrtParser_tracks, "f").size;
+                    const TrackList = {
+                        index: length,
+                        _track_: TEXT_T
+                    };
+                    __classPrivateFieldGet(this, _SrtParser_track_lists, "f").push(TrackList);
+                    const track = {
+                        name: Track.name,
+                        label: Track.label,
+                        language: Track.language,
+                        default: Track.default,
+                        url: Track.url,
+                        data
+                    };
+                    __classPrivateFieldGet(this, _SrtParser_tracks, "f").set(length, track);
+                }));
+            }
+            catch (_a) {
+                throw Error("object does not have expected patterns!");
+            }
+        });
+    }
+    request(url) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const res = yield fetch(url);
+            return yield res.text();
+        });
+    }
+    append_media(media) {
+        __classPrivateFieldSet(this, _SrtParser_media, media, "f");
     }
     GetKey(key) {
         return __classPrivateFieldGet(this, _SrtParser_caption, "f").get(String(key));
     }
+    GetKeyFromTime(time, delimiter) {
+    }
     run() {
         return __awaiter(this, void 0, void 0, function* () {
-            const input = __classPrivateFieldGet(this, _SrtParser_input, "f");
-            const regex = /(\r\n|\r|\n)/g;
-            const sliced_input = input.split(regex).filter(slice => { return !slice.match(/(\r\n|\r|\n)/g) && slice != ""; });
-            let number = -1;
-            let i = 0;
-            let _caption;
-            const array_regex = [/^\d+$/, /^(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})$/];
-            while (i < sliced_input.length) {
-                if (array_regex[0].test(sliced_input[i])) {
-                    if (number !== -1 || i == sliced_input.length - 1) {
-                        __classPrivateFieldGet(this, _SrtParser_caption, "f").set(String(number), _caption);
+            const mediaElement = __classPrivateFieldGet(this, _SrtParser_media, "f");
+            const textTracks = mediaElement.textTracks;
+            let _this = this;
+            mediaElement.textTracks.addEventListener('change', (e) => {
+                const textTracks = mediaElement.textTracks;
+                for (let i = 0; i < textTracks.length; i++) {
+                    const track = textTracks[i];
+                    if (track.mode === 'showing' && __classPrivateFieldGet(this, _SrtParser_current, "f") != i) {
+                        const find = __classPrivateFieldGet(this, _SrtParser_tracks, "f").get(Number(i));
+                        if (!(find === null || find === void 0 ? void 0 : find.data))
+                            break;
+                        clearAllTextTracks(mediaElement);
+                        __classPrivateFieldSet(this, _SrtParser_caption, find === null || find === void 0 ? void 0 : find.data, "f");
+                        __classPrivateFieldSet(this, _SrtParser_current, i, "f");
+                        init_caption();
+                        break;
                     }
-                    number = Number(sliced_input[i]);
-                    _caption = {
-                        init: '',
-                        end: '',
-                        captions: []
-                    };
                 }
-                else if (array_regex[1].test(sliced_input[i])) {
-                    const match = sliced_input[i].match(array_regex[1]);
-                    _caption.init = match[1];
-                    _caption.end = match[2];
+            });
+            function run_std_out(final, currentTime) {
+                var _a;
+                let captions = [];
+                const filter = final.filter(c => c.init / 1000 <= currentTime && c.end / 1000 >= currentTime);
+                if (filter.length == 0) {
+                    __classPrivateFieldGet(_this, _SrtParser__stdout, "f").innerText = "";
+                    return;
                 }
-                else {
-                    _caption.captions.push(sliced_input[i]);
-                }
-                i += 1;
-                if (number !== -1) {
-                    __classPrivateFieldGet(this, _SrtParser_caption, "f").set(String(number), _caption);
+                captions = (_a = __classPrivateFieldGet(_this, _SrtParser_caption, "f").get(String(filter[0].index))) === null || _a === void 0 ? void 0 : _a.captions.reverse();
+                __classPrivateFieldGet(_this, _SrtParser__stdout, "f").innerText = captions.join('\n');
+            }
+            function clearCuesFromTrack(track) {
+                if (!track)
+                    throw Error("Caption not found!");
+                if (track.cues) {
+                    const cues = track.cues;
+                    while (cues.length > 0) {
+                        track.removeCue(cues[0]);
+                    }
                 }
             }
+            function clearAllTextTracks(videoElement) {
+                const textTracks = videoElement.textTracks;
+                for (let i = 0; i < textTracks.length; i++) {
+                    clearCuesFromTrack(textTracks[i]);
+                }
+            }
+            function convert_to_number(label) {
+                const regex = /(\d{2}):(\d{2}):(\d{2}),(\d{3})/g;
+                let match;
+                const groups = [];
+                while ((match = regex.exec(label)) !== null) {
+                    groups.push({
+                        hours: Number(match[1]),
+                        minutes: Number(match[2]),
+                        seconds: Number(match[3]),
+                        milliseconds: Number(match[4])
+                    });
+                }
+                return groups;
+            }
+            function init_caption() {
+                var _a;
+                const captions = Array.from(__classPrivateFieldGet(_this, _SrtParser_caption, "f").values());
+                let index = 1;
+                let final_captions = [];
+                for (const e of captions) {
+                    const time_reffer = { init: convert_to_number(e.init), end: convert_to_number(e.end) };
+                    const from_sec = [(time_reffer.init[0].hours * 60 + time_reffer.init[0].minutes) * 60 + time_reffer.init[0].seconds, (time_reffer.end[0].hours * 60 + time_reffer.end[0].minutes) * 60 + time_reffer.end[0].seconds];
+                    const from_millis = from_sec.map((sec, index) => {
+                        return (index == 0) ? Number((sec * 1000) + time_reffer.init[0].milliseconds) : Number((sec * 1000) + time_reffer.end[0].milliseconds);
+                    });
+                    const caption_mapped_ = {
+                        init: from_millis[0],
+                        end: from_millis[1],
+                        index: index
+                    };
+                    final_captions.push(caption_mapped_);
+                    index += 1;
+                }
+                const find = __classPrivateFieldGet(_this, _SrtParser_track_lists, "f").find(e => e.index == __classPrivateFieldGet(_this, _SrtParser_current, "f"));
+                let caption_from_media = find === null || find === void 0 ? void 0 : find._track_;
+                if (!caption_from_media)
+                    return;
+                if (__classPrivateFieldGet(_this, _SrtParser__stdout, "f")) {
+                    function timeUpdateHandler() {
+                        run_std_out(final_captions, mediaElement.currentTime);
+                    }
+                    mediaElement.removeEventListener('timeupdate', timeUpdateHandler);
+                    mediaElement.addEventListener('timeupdate', timeUpdateHandler);
+                }
+                caption_from_media.mode = "showing";
+                for (const track of final_captions) {
+                    (_a = __classPrivateFieldGet(_this, _SrtParser_caption, "f").get(String(track.index))) === null || _a === void 0 ? void 0 : _a.captions.reverse().forEach((c, i) => {
+                        const init = track.init / 1000;
+                        const end = track.end / 1000;
+                        caption_from_media.addCue(new VTTCue(init, end, String(c)));
+                    });
+                }
+            }
+            /* this.#Process();
+            
+    let caption_from_media:TextTrack = media.addTextTrack("captions", "Caption", "pt-BR");
+    caption_from_media.mode = "showing";
+             for(const track of final_captions){
+                this.#caption.get(String(track.index))?.captions.reverse().forEach((c,i)=>{
+                    console.log(i,c,track.init/1000)
+                    const init = track.init/1000;
+                    const end = track.end/1000;
+                    caption_from_media.addCue(new VTTCue(init, end, String(c)));
+                })
+             }*/
         });
     }
 }
-_SrtParser_input = new WeakMap(), _SrtParser_caption = new WeakMap();
+_SrtParser_input = new WeakMap(), _SrtParser_current = new WeakMap(), _SrtParser_caption = new WeakMap(), _SrtParser_tracks = new WeakMap(), _SrtParser__stdout = new WeakMap(), _SrtParser_media = new WeakMap(), _SrtParser_track_lists = new WeakMap(), _SrtParser_instances = new WeakSet(), _SrtParser_Process = function _SrtParser_Process() {
+    const input = __classPrivateFieldGet(this, _SrtParser_input, "f");
+    const res = new Map();
+    const regex = /(\r\n|\r|\n)/g;
+    if (!input)
+        return false;
+    const sliced_input = input.split(regex).filter(slice => { return !slice.match(regex) && slice != ""; });
+    let number = -1;
+    let i = 0;
+    let _caption;
+    const array_regex = [/^\d+$/, /^(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})$/];
+    while (i < sliced_input.length) {
+        if (array_regex[0].test(sliced_input[i])) {
+            if (number !== -1 || i == sliced_input.length - 1) {
+                res.set(String(number), _caption);
+            }
+            number = Number(sliced_input[i]);
+            _caption = {
+                init: '',
+                end: '',
+                captions: []
+            };
+        }
+        else if (array_regex[1].test(sliced_input[i])) {
+            const match = sliced_input[i].match(array_regex[1]);
+            _caption.init = match[1];
+            _caption.end = match[2];
+        }
+        else {
+            _caption.captions.push(sliced_input[i]);
+        }
+        i += 1;
+        if (number !== -1) {
+            res.set(String(number), _caption);
+        }
+    }
+    return res;
+};
--- srtparser.ts
+++ srtparser.ts
@@ -1,25 +1,93 @@
-
 interface caption{
     init:string,
     end:string,
-    captions:Array<string>
+    captions:Array<string>,
 }
+interface TimeGroup {
+    hours: number;
+    minutes: number;
+    seconds: number;
+    milliseconds: number;
+  }
+  interface CaptionMapped {
+    init:number,
+    end:number,
+    index:number
+    }
+
+    interface Track{
+        name:string,
+        language:string,
+        label:string,
+        default:boolean,
+        url:string,
+        data:Map<string,caption> | boolean
+    }
+    interface Text_Track{
+        index:number,
+        _track_:TextTrack
+    }
 export class SrtParser{
     #input:string;
-    #caption:Map<string,caption> = new Map()
-    constructor(input:string){
-     this.#input = input;
+    #current:number;
+    #caption:Map<string,caption>  = new Map()
+    #tracks:Map<number,Track> = new Map();
+    #_stdout:HTMLElement;
+    #media:HTMLVideoElement | HTMLAudioElement;
+    #track_lists:Array<Text_Track> = [];
+
+    set stdout(elem:HTMLElement){
+    this.#_stdout = elem;
     }
-    lexer(){
+    async SetCaptions(TrackObject:Array<Track>){
+        try{
+             TrackObject.forEach(async (Track:Track)=>{
+                console.log(Track.url)
+                const res = await this.request(Track.url)
+                this.#input = res;
+                let data:Map<string,caption> | boolean = this.#Process();
+                const media = this.#media;
+                const TEXT_T:TextTrack = media.addTextTrack("captions", Track.label, Track.language);
+                const length = this.#tracks.size;
+                const TrackList:Text_Track = {
+                    index:length,
+                   _track_:TEXT_T
+                }
+                this.#track_lists.push(TrackList)
+                const track:Track = {
+                    name:Track.name,
+                    label:Track.label,
+                    language:Track.language,
+                    default:Track.default,
+                    url:Track.url,
+                    data
+                }
+                this.#tracks.set(length,track)
+                
+             })
+            
+            
+            
+           }
+          catch{
+            throw Error("object does not have expected patterns!")
+          }
+    }
+    async request(url:string){
+    const res = await fetch(url)
+    return await res.text()
+    }
+
+     append_media(media:HTMLVideoElement | HTMLAudioElement){
+      this.#media = media;
+     }
+
+     #Process():Map<string,caption> | boolean{
         const input = this.#input;
-    }
-    GetKey(key:number){
-        return this.#caption.get(String(key))
-    }
-    async run(){
-       const input = this.#input;
+        const res:Map<string,caption> = new Map()
        const regex = /(\r\n|\r|\n)/g
-       const sliced_input = input.split(regex).filter(slice=> {return !slice.match(/(\r\n|\r|\n)/g) && slice != ""});
+       if(!input)return false
+       const sliced_input = input.split(regex).filter(slice=> {return !slice.match(regex) && slice != ""});
        let number = -1;
        let i = 0;
        let _caption: caption;
@@ -27,7 +95,7 @@ export class SrtParser{
        while(i < sliced_input.length){
         if(array_regex[0].test(sliced_input[i])){
         if(number !== -1 || i == sliced_input.length-1){
-            this.#caption.set(String(number),_caption!);
+            res.set(String(number),_caption!);
         }
         
         number = Number(sliced_input[i])
@@ -49,8 +117,129 @@ export class SrtParser{
         }
         i+=1
         if (number !== -1) {
-            this.#caption.set(String(number), _caption!);
+            res.set(String(number), _caption!);
           }
        }
+       return res;
+    }
+    GetKey(key:number){
+        return this.#caption.get(String(key))
+    }
+    GetKeyFromTime(time:string | number,delimiter:string | number){
+      
+    }
+    async run(){
+        const mediaElement = this.#media;
+        const textTracks = mediaElement.textTracks;
+        let _this = this;
+        mediaElement.textTracks.addEventListener('change', (e) => {
+            const textTracks = mediaElement.textTracks;
+            for (let i = 0; i < textTracks.length; i++) {
+                const track = textTracks[i];
+                if (track.mode === 'showing' && this.#current != i) {
+                    const find = this.#tracks.get(Number(i))
+                    if(!find?.data)break
+                    clearAllTextTracks(mediaElement)
+                    this.#caption = find?.data as Map<string,caption>;
+                    this.#current = i;
+                    init_caption();
+                    
+                    break
+                }
+            }
+        });
+        function run_std_out(final: Array<CaptionMapped>,currentTime:number) {
+            let captions:Array<string> | undefined = [];
+            const filter = final.filter(c=>c.init/1000 <= currentTime && c.end/1000 >= currentTime);
+            if(filter!.length == 0){
+                _this.#_stdout.innerText = "";
+                return
+            }
+            captions = _this.#caption.get(String(filter[0].index))?.captions.reverse();
+           _this.#_stdout.innerText  = captions!.join('\n');
+    }
+        function clearCuesFromTrack(track: TextTrack | undefined) {
+            if(!track) throw Error("Caption not found!")
+            if (track.cues) {
+                const cues = track.cues;
+                while (cues.length > 0) {
+                    track.removeCue(cues[0]);
+                }
+            }
+        }
+        function clearAllTextTracks(videoElement: HTMLMediaElement) {
+            const textTracks = videoElement.textTracks;
+            for (let i = 0; i < textTracks.length; i++) {
+                clearCuesFromTrack(textTracks[i]);
+            }
+        }
+        function convert_to_number(label:string):TimeGroup []{
+            const regex = /(\d{2}):(\d{2}):(\d{2}),(\d{3})/g;
+            let match;
+            const groups:TimeGroup[] = [];
+            
+            while ((match = regex.exec(label)) !== null) {
+                groups.push({
+                    hours: Number(match[1]),
+                    minutes: Number(match[2]),
+                    seconds: Number(match[3]),
+                    milliseconds: Number(match[4])
+                });
+            }
+            return groups;
+        }
+        
+        function init_caption(){
+            const captions = Array.from(_this.#caption.values());            
+             let index = 1;
+             let final_captions:Array<CaptionMapped> = [];
+             for(const e of captions){
+                const time_reffer = {init:convert_to_number(e.init),end:convert_to_number(e.end)}
+                const from_sec = [(time_reffer.init[0].hours*60 + time_reffer.init[0].minutes) *60 + time_reffer.init[0].seconds,(time_reffer.end[0].hours*60 + time_reffer.end[0].minutes) *60 + time_reffer.end[0].seconds];
+                const from_millis = from_sec.map((sec,index)=>{
+                    return (index == 0) ? Number((sec * 1000)+time_reffer.init[0].milliseconds) : Number((sec * 1000)+time_reffer.end[0].milliseconds)
+                })
+                const caption_mapped_: CaptionMapped = {
+                    init: from_millis[0], 
+                    end: from_millis[1], 
+                    index: index  
+                };
+            
+                final_captions.push(caption_mapped_);
+                index += 1; 
+             }
+             const find = _this.#track_lists.find(e=>e.index == _this.#current);
+             let caption_from_media:TextTrack | undefined = find?._track_;
+             if(!caption_from_media)return
+             if(_this.#_stdout){
+                function timeUpdateHandler() {
+                    run_std_out(final_captions,mediaElement.currentTime);
+                }
+                mediaElement.removeEventListener('timeupdate',timeUpdateHandler)
+                mediaElement.addEventListener('timeupdate',timeUpdateHandler)
+             }
+             caption_from_media.mode = "showing";
+         
+             for(const track of final_captions){
+            _this.#caption.get(String(track.index))?.captions.reverse().forEach((c,i)=>{
+                const init = track.init/1000;
+                const end = track.end/1000;
+
+                caption_from_media.addCue(new VTTCue(init, end, String(c)));
+            })
+         }
+        }
+        /* this.#Process();
+        
+let caption_from_media:TextTrack = media.addTextTrack("captions", "Caption", "pt-BR");
+caption_from_media.mode = "showing";
+         for(const track of final_captions){
+            this.#caption.get(String(track.index))?.captions.reverse().forEach((c,i)=>{
+                console.log(i,c,track.init/1000)
+                const init = track.init/1000;
+                const end = track.end/1000;
+                caption_from_media.addCue(new VTTCue(init, end, String(c)));
+            })
+         }*/
     }
 }
